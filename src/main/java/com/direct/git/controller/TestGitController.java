7b5a63ce84cfd67092f00b652cee627737350fac
admin <tom@direct.com>
2022-06-15 15:00:10 +0800

    first_pay one commit

--- src/main/java/com/direct/git/controller/TestGitController.java
+++ src/main/java/com/direct/git/controller/TestGitController.java
@@ -3,5 +3,6 @@ package com.direct.git.controller;
 public class TestGitController {
     public static void main(String[] args) {
         System.out.println("git------------------------git");
+        System.out.println("first_git------------------------first_git");
     }
 }
