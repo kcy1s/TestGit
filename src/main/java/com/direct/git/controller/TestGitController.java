450e317c2cf4e0a8fb3c0e19432a47a437874d9d
admin <tom@direct.com>
2022-06-15 14:41:09 +0800

    first commit

---
+++ src/main/java/com/direct/git/controller/TestGitController.java
@@ -0,0 +1,7 @@
+package com.direct.git.controller;
+
+public class TestGitController {
+    public static void main(String[] args) {
+        System.out.println("git------------------------git");
+    }
+}
