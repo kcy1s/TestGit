ecc318c065d132ac3af6cd69aa36d276ee61a6df
admin <tom@direct.com>
2022-06-15 15:03:48 +0800

    first_pay two commit

--- src/main/java/com/direct/git/controller/TestGitController.java
+++ src/main/java/com/direct/git/controller/TestGitController.java
@@ -4,5 +4,6 @@ public class TestGitController {
     public static void main(String[] args) {
         System.out.println("git------------------------git");
         System.out.println("first_git------------------------first_git");
+        System.out.println("two_git------------------------two_git");
     }
 }
