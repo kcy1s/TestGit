1791d69f9ab69fc934995e0d2af226d84604abd4
admin <tom@direct.com>
2022-06-15 14:50:40 +0800

    two commit

--- src/main/java/com/direct/git/controller/TestGitController.java
+++ src/main/java/com/direct/git/controller/TestGitController.java
@@ -3,5 +3,7 @@ package com.direct.git.controller;
 public class TestGitController {
     public static void main(String[] args) {
         System.out.println("git------------------------git");
+        System.out.println("gitTwo------------------------gitTwo");
+
     }
 }
